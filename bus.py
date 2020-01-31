48bc0b0c097163fe980822dd7d1398376f98ec0e
avinash166-til <54021865+avinash166-til@users.noreply.github.com>
2020-01-31 22:49:43 +0530

    Add files via upload

---
+++ bus.py
@@ -0,0 +1,14 @@
+for i in range(1,8):
+     for j in range(1,5):
+          if (i==1 and j==2) or (i==3 and j==4) or (i==6 and j==3) or (i==6 and j==1) or (i==3 and j==3):
+               print("#",end='')
+          else:
+               print("$",end='')
+     print()
+     
+
+
+
+
+        
+     
